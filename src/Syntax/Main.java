85bdaa51f845d80e766950326144766540089fa9
bankaiop7 <bankaiop@mail.ru>
2025-03-10 01:07:54 +0300

    New

--- src/Syntax/Main.java
+++ src/Syntax/Main.java
@@ -1,10 +1,16 @@
 package Syntax;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/**
+ * @author
+ * @version
+ */
+
 public class Main {
+    /*
+      //this method "main" is entry point to our program
+       */
     public static void main(String[] args) {
-        //
+        System.out.println("Hello World");
+
         }
     }
-}

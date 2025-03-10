2348596584cfa3793703d1d15a971794f2bc531c
bankaiop7 <bankaiop@mail.ru>
2025-03-10 15:36:44 +0300

    New

--- src/Syntax/lesson5_variable/Variables.java
+++ src/Syntax/lesson5_variable/Variables.java
@@ -4,7 +4,7 @@ public class Variables {
     public static void main(String[] args) {
 //Byte -128..128
 //System.out.println(Integer.parseInt("11111111",2));
-Byte byteNumber= 127;
+Byte byteNumber= 126;
         System.out.println("The byte number: "+byteNumber);
 //short -32768...32767
         short shortNumber=500;
@@ -44,6 +44,22 @@ Byte byteNumber= 127;
         int firstNumber, secondNumber;
         firstNumber=5;
         secondNumber=10;
+
+        //casting
+            //bytenumber=127
+            short varF0r_Casting=byteNumber;
+            System.out.println("After casting varForCasting:"+varF0r_Casting);
+
+            int intNewVar=varF0r_Casting;
+            System.out.println("After casting IntNewVar:"+varF0r_Casting);
+
+            int intVariable=6_000_012;
+            byte byteNewVar= (byte) intVariable;
+            System.out.println("After casting byteNewVar:"+byteNewVar);
+
+            var doubleVar=4.18;
+            int newInteger = (int) doubleVar;
+            System.out.println("After casting newInteger:"+newInteger);
     }
 
 }

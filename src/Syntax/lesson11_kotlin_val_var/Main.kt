afedfccb53dec09d8975468b784dbc8cfaa27777
bankaiop7 <bankaiop@mail.ru>
2025-03-12 16:43:19 +0300

    New

--- src/Syntax/lesson11_kotlin_val_var/Main.kt
+++ src/Syntax/lesson11_kotlin_val_var/Main.kt
@@ -1,2 +1,19 @@
 package Syntax.lesson11_kotlin_val_var
 
+fun main() {
+    //java
+ // int nameOfVariable=5
+
+    //kotlin
+    var intNumber:Int=5
+    var someString:String="My name is Dima Ten"
+
+    intNumber=4
+val nameOfManager="Dima Ten"
+    nameOfManager="Dima"
+
+
+
+
+
+}
---
+++ src/Syntax/lesson12_string_templates/Main.kt
@@ -0,0 +1,10 @@
+package Syntax.lesson12_string_templates
+
+fun main() {
+    val nameOfManager="Dima Ten"
+    println("Hello, $nameOfManager")
+
+
+
+
+}
